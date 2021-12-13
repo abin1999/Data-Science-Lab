221cb8828b025012ee66f078dd4b36248e88fff6
abin1999 <74650927+abin1999@users.noreply.github.com>
2021-12-13 12:41:33 +0530

    Add files via upload

---
+++ cycle 2/C02P01.py
@@ -0,0 +1,9 @@
+
+
+import numpy as np
+
+a_3d_array = np.array([[[1, 2,5], [3, 4,6]], [[5, 6,2], [7, 8.6,9]]],dtype="f")
+
+print(a_3d_array.dtype)
+
+print(a_3d_array)
---
+++ cycle 2/C02P03.py
@@ -0,0 +1,13 @@
+#3. Familiarize with the functions to create
+#a) an uninitialized array
+#b) array with all elements as 1,
+#c) all elements as 0
+
+import numpy as np
+
+print("an uninitialized array :")
+print(np.empty([2,2]))
+print("array with all elements as 1 :")
+print(np.full([2,2],1))
+print("all elements as 0 :")
+print(np.zeros([2,2]))
